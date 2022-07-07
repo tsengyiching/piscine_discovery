b1af5c649b8b1bf4d1d63698b0e09e77cad07eca
Yi-Ching <yictseng@student.42.fr>
2022-07-07 17:59:49 +0200

    cell33: todo add ok

--- cell33/ex03/todo.js
+++ cell33/ex03/todo.js
@@ -1,24 +1,50 @@
 function handleClick() {
+  let todos = getTodos();
+  let lastId;
+  let text = prompt("Whant needs to be done ?");
+
+  if (!todos) {
+    todos = [];
+    lastId = 0;
+  } else {
+    lastId = Number(document.getElementsByClassName("todo")[0].id) + 1;
+  }
+  if (!text) return;
+
+  const newTodo = { key: lastId, text: text };
+  todos.push(newTodo);
+  document.cookie = `todos` + `=` + JSON.stringify(todos);
+  renderTodo(newTodo);
+}
+
+function renderTodo(elem) {
   const list = document.getElementById("ft_list");
-  let todo = document.createElement("div");
-  todo.classList.add("todo");
-  todo.setAttribute("id", "todo");
 
-  //   console.log(todo);
-  const para = document.createElement("p");
-  const node = document.createTextNode("This is new.");
+  let div = document.createElement("div");
+  div.classList.add("todo");
+  div.setAttribute("id", elem.key);
 
+  let para = document.createElement("p");
+  let node = document.createTextNode(elem.text);
   para.appendChild(node);
-  todo.appendChild(para);
-  list.appendChild(todo);
-  // let todo
+  div.appendChild(para);
+  list.prepend(div);
+}
 
-  console.log(list);
+function getTodos() {
+  let todos = document.cookie;
 
-  console.log("here: ", document.getElementById("ft_list"));
+  if (!todos) return;
 
-  //   let text = prompt("Whant needs to be done ?");
-  //   if (text) {
-  //     console.log(text);
-  //   }
+  todos = todos.split("=");
+  todos = JSON.parse(todos[1]);
+
+  return todos;
+}
+
+function firstTodoRender() {
+  let todos = getTodos();
+
+  if (!todos) return;
+  todos.forEach((elem) => renderTodo(elem));
 }
